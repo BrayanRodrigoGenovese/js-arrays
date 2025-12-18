60c4741cf1365ff7236f06a4437558b55dca2627
Brayan <bittolo.8bit@gmail.com>
2025-12-18 14:07:26 +0100

    fourth exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,7 +15,8 @@ const lastTeacher = teachers.pop();
 console.log(`l'ultimo insegnante era: ${lastTeacher}`);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(`il primo insegnante era: ${firstTeacher}`);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
